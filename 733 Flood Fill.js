86aeb078a36cf8c6575ca257f49db7e015f38aba
Jon Yang <jonyang@jondembp.lan>
2020-07-27 22:19:46 -0400

    Updated om July 27

--- 733 Flood Fill.js
+++ 733 Flood Fill.js
@@ -33,6 +33,41 @@ var floodFill = function(image, sr, sc, newColor) {
     return image
 };
 
+// 第二次做
+
+var floodFill = function(image, sr, sc, newColor) {
+    let visited = new Set()
+    // 萬一 newColor 與 original color 同值，會出現 stack overflow
+    
+    const dfs  = (row, col, value, newColor, image) => {
+        value = (value === undefined) ? image[row][col] : value
+        // 用 value = value || image[row][col]，若 image[row][col] = 0 會都是 false
+        
+        let position = `${row}_${col}`
+        
+        if (row < 0 || col < 0 || row >= image.length || col >= image[0].length) {
+            return; 
+        }
+        if (image[row][col] !== value) {
+            return;
+        }
+        if (visited.has(position)) {return}
+
+        visited.add(position)
+        image[row][col] = newColor;
+
+        dfs(row + 1, col, value, newColor, image);
+        dfs(row - 1, col, value, newColor, image);
+        dfs(row, col + 1, value, newColor, image);
+        dfs(row, col - 1, value, newColor, image);
+    }
+
+    dfs(sr, sc, undefined, newColor, image);
+    return image;
+};
+
+
+
 const image = [[1,1,1],[1,1,0],[1,0,1]]
 const sr = 1
 const sc = 1
