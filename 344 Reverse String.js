d574b11fb017f69be521d6299baf17b127eeedb5
James Yang <ycyang218@gmail.com>
2020-09-08 08:43:36 -0400

    Updated on Sep 7

--- 344 Reverse String.js
+++ 344 Reverse String.js
@@ -13,3 +13,21 @@ var reverseString = function(s) {
     }
     return s
 };
+
+// 第二次做
+
+var reverseString = function(s) {
+    let left = 0
+    let right = s.length - 1
+
+    while (left < right) {
+        [s[left], s[right]] = [s[right], s[left]]
+        left++
+        right--
+    }
+    return s
+}
+
+const s = ["h","e","l","l","o"]
+
+console.log(reverseString(s))
--- 42 Trapping Rain Water.js
+++ 42 Trapping Rain Water.js
@@ -36,3 +36,38 @@ var trap = function(height) {
 const height = [0,1,0,2,1,0,1,3,2,1,2,1]
 
 console.log(trap(height))
+
+// 第二次做
+
+var trap = function(height) {
+    const left = []
+    const right = []
+
+    for (let i = 0; i < height.length; i++) {
+        if (i === 0) {
+            left.push(0)
+        } else {
+            left.push(Math.max(left[left.length - 1], height[i - 1]))
+        }
+        if (height.length - 1 - i === height.length - 1) {
+            right.push(0)
+        } else {
+            right.push(Math.max(right[right.length - 1], height[height.length - i]))
+        }
+    }
+
+    console.log(left, right)
+    right.reverse()
+    let trappedRain = 0
+
+    for (let i = 0; i < height.length; i++) {
+        if (Math.min(left[i], right[i]) - height[i] > 0) {
+            trappedRain += Math.min(left[i], right[i]) - height[i]
+        }
+    }
+    return trappedRain
+}
+
+const height = [2,0,2]
+
+console.log(trap(height))
